315a939f4822e958b1394eacd7a2f385d2860076
viglino <jean-marc.viglino@ign.fr>
2022-05-06 17:50:15 +0200

    [UPD]

--- src/game/story.js
+++ src/game/story.js
@@ -1,3 +1,7 @@
+import DropFile from 'ol-ext/interaction/DropFile'
+import dialog from "../map/dialog";
+import map from "../map/map";
+
 const data = {
   ready: false,
   stories: {}
@@ -19,44 +23,83 @@ loader.forEach(s => {
   fetch('./story/' + s + '.txt')
   .then(response => response.text())
   .then(st => {
-    st = st.replace(/\r/g, '').split('\n');
-    const story = {};
-    stories[s] = story;
-    let key;
-    st.forEach(l => {
-      if (l.charAt(0) === '#') {
-        l = l.split(':');
-        key = l.shift().replace(/^#/,'').trim();
-        const val = l.join(':');
-        if (l.length) {
-          if (story[key]) {
-            if (!(story[key] instanceof Array)) story[key] = [story[key]];
-            story[key].push(val.trim());
-          } else {
-            story[key] = val.trim();
-          }
-        }
-      } else if (key) {
-        if (story[key] instanceof Array) {
-          const n = story[key].length -1
-          story[key][n] += (story[key][n] ? '\n' : '') + l;
-        } else {
-          story[key] += (story[key] ? '\n' : '') + l;
-        }
-      }
-    })
+    stories[s] = decode(st);
     data.length = Object.keys(stories).length;
     data.ready = (data.length === loader.length);
+    if (data.ready) {
+      // Get story from url
+      dropStory = stories[location.search.replace(/^\?/,'')];
+    }
   });
 })
 
+/** Decode story */
+function decode(st) {
+  st = st.replace(/\r/g, '').split('\n');
+  const story = {};
+  let key;
+  st.forEach(l => {
+    if (l.charAt(0) === '#') {
+      l = l.split(':');
+      key = l.shift().replace(/^#/,'').trim();
+      const val = l.join(':');
+      if (l.length) {
+        if (story[key]) {
+          if (!(story[key] instanceof Array)) story[key] = [story[key]];
+          story[key].push(val.trim());
+        } else {
+          story[key] = val.trim();
+        }
+      }
+    } else if (key) {
+      if (story[key] instanceof Array) {
+        const n = story[key].length -1
+        story[key][n] += (story[key][n] ? '\n' : '') + l;
+      } else {
+        story[key] += (story[key] ? '\n' : '') + l;
+      }
+    }
+  })
+  return story;
+}
+
 /** Get a random story */
 function getRandomStory() {
+  dropFile.setActive(false);
+  if (dropStory) {
+    return dropStory;
+  }
   const keys = Object.keys(stories);
   const k = keys[Math.floor(Math.random() * keys.length)] || 'default';
   console.log(k)
   return stories[k];
 }
+
+// Story dropped
+let dropStory;
+
+const dropFile = new DropFile({
+  formatConstructors: [],
+  accept: ['.txt']
+})
+map.addInteraction(dropFile)
+
+// A story file is dropped
+dropFile.on('loadstart', (e) => {
+  if (dropFile.getActive() && e.isok) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const result = e.target.result;
+      if (result) {
+        dropStory = decode(result)
+        dialog.hide();
+      }
+    };
+    // Start loading
+    reader.readAsText(e.file);
+  }
+})
+
 
 export { getRandomStory }
 
